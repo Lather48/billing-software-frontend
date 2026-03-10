9f2a7266107bc5a65b7b39f105e4b72aaa726ad9
Robin Lather <robinlather20@gmail.com>
2026-03-10 15:05:58 +0530

    Update API URLs in AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,7 +11,7 @@ export const AuthProvider = ({ children }) => {
             if (token) {
                 try {
                     axios.defaults.headers.common['x-auth-token'] = token;
-                    const res = await axios.get('http://16.171.253.179:5000/api/auth/me');
+                    const res = await axios.get('https://bilabiate-sharyl-noncriminally.ngrok-free.dev/api/auth/me');
                     setUser(res.data.user);
                 } catch (err) {
                     localStorage.removeItem('token');
@@ -23,14 +23,14 @@ export const AuthProvider = ({ children }) => {
         checkUser();
     }, []);
     const login = async (email, password) => {
-        const res = await axios.post('http://16.171.253.179:5000/api/auth/login', { email, password });
+        const res = await axios.post('https://bilabiate-sharyl-noncriminally.ngrok-free.dev/api/auth/login', { email, password });
         localStorage.setItem('token', res.data.token);
         axios.defaults.headers.common['x-auth-token'] = res.data.token;
         setUser(res.data.user);
         return res.data;
     };
     const register = async (formData) => {
-        const res = await axios.post('http://16.171.253.179:5000/api/auth/register', formData);
+        const res = await axios.post('https://bilabiate-sharyl-noncriminally.ngrok-free.dev/api/auth/register', formData);
         localStorage.setItem('token', res.data.token);
         axios.defaults.headers.common['x-auth-token'] = res.data.token;
         setUser(res.data.user);
